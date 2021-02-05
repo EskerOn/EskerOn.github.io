bf1e454adddb864be3d2f5c8d7a353eedcb448fa
EskerOn <35613070+EskerOn@users.noreply.github.com>
2021-02-05 16:40:23 -0600

    complete project

---
+++ project2/assets/scripts/main.js
@@ -0,0 +1,42 @@
+//el dun document
+//query selector regresa 1 sólo elemento
+//let container = document.querySelector('.container');
+
+// let links = document.querySelectorAll('a');
+
+// links.forEach(function(link){
+// console.log(link);
+// });
+
+let celdas = document.querySelectorAll('td');
+
+celdas.forEach(function (td) {
+    td.addEventListener('click', function () {
+        console.log(this)
+    });
+});
+
+let closes = document.querySelectorAll('.close');
+
+closes.forEach(function (close) {
+    close.addEventListener('click', function (ev) {
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+        content.classList.remove('animate__zoomIn');
+        content.classList.remove('animate__animated');
+        content.classList.add('animate__zoomOut');
+        content.classList.add('animate__animated');
+        //setTimeout setInterval
+        setTimeout(function(){
+            location.href = "/";
+        }, 300);  
+        
+        console.log(':c bien sadboy xdxdxd');
+    });
+});
+
+// let iconos = document.querySelectorAll('i');
+
+// iconos.forEach(function(icono){
+//     icono.classList.remove('fa-star');
+// });
