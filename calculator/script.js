4d298ad72eef7bae146cb0b3940218a95c10fc21
Alejandro Escalante Hernandez <alejandro.escalante@alumno.buap.mx>
2021-04-14 19:39:00 -0500

    converting to pwa

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,8 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('worker.js?'+Math.random()+'')
+    });
+  }
 //function that display value
 function addval(val){
     document.getElementById("result").value += val
